9ada1e597a921dca85b115581349efa5d7ad5831
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2025-03-28 15:26:07 +0000

    NewsLetter Component

--- src/components/home/banner/Banner.tsx
+++ src/components/home/banner/Banner.tsx
@@ -7,7 +7,7 @@ export const Banner = () => {
         {/* Imagen de fondo */}
         <div
             className="absolute inset-0 bg-cover bg-center opacity-70 h-full"
-            style={{ backgroundImage: 'url(/img/img-banner.jpg)' }}
+            style={{ backgroundImage: 'url(/img/background-newsletter.webp)' }}
         />
         
         {/* Overlay */}
@@ -17,7 +17,7 @@ export const Banner = () => {
         <div className='relative z-10 flex flex-col items-center justify-center py-20 px-4 text-center lg:py-40 lg:px-8'>
         <h1 className="text-4xl font-bold mb-4 lg:text-6xl">Welcome to our store</h1>
         <p className="mt-4 text-lg lg:text-2xl">The best place to buy your electronics</p>
-        <Link to="/celulares" className="mt-8 bg-white text-gray-900 font-semibold px-6 py-2 rounded-full">Shop Now</Link>
+        <Link to="/celulares" className="mt-8 bg-cyan-600 text-white font-semibold px-6 py-2 rounded-full">Shop Now</Link>
       </div>
     </div>
   )
--- src/components/home/newsletter/NewsLetter.tsx
+++ src/components/home/newsletter/NewsLetter.tsx
@@ -1,8 +1,34 @@
-
 export const NewsLetter = () => {
   return (
-    <div>
-      <h1>newsletter</h1>
+    <div className="relative bg-gray500 text-white py-20">
+      {/** Imagen de fondo */}
+      <div
+        className="absolute inset-0 bg-cover opacity-70 h-full"
+        style={{ backgroundImage: "url(/img/background-newsletter.webp)" }}
+      />
+      {/** Contenido del newsletter */}
+      <div className="container z-10 relative p-5 md:p-0">
+        <div className="w-full text-black bg-white p-12 space-y-5 md:w-[50%] lg:w-[40%]">
+          <p className="text-xs uppercase font-semibold">
+            Subscribe to our newsletter
+          </p>
+          <p className="text-xs font-medium w-[%80]leading-5">
+            Get the latest news and updates about our products, promotions, and
+            more. Don't miss out on exclusive offers and discounts. Subscribe
+            now!
+          </p>
+          <form className="flex flex-col gap-5 xl:flex-row">
+            <input
+              type="email"
+              className="border border-slate-200 focus:outline-none rounded-md py-5 w-full text-xs font-medium"
+              placeholder="Enter your email address"
+            />
+            <button className="bg-cyan-600 px-4 py-3 rounded-md text-white uppercase xl:py-5">
+              Subscribe
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
